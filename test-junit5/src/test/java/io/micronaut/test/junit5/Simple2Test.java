5d39183ef766c7320251ebe72aba492bf066eb18
Anze Sodja <anze.sodja@outfit7.com>
2020-09-13 16:05:29 +0200

    Show that mocking from base does not work

--- test-junit5/src/test/java/io/micronaut/test/junit5/Simple2Test.java
+++ test-junit5/src/test/java/io/micronaut/test/junit5/Simple2Test.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
 
 @MicronautTest
 public class Simple2Test extends SimpleBaseTest {
@@ -16,5 +18,6 @@ public class Simple2Test extends SimpleBaseTest {
     @Test
     void testComputeNumToSquare() {
         assertNotNull(simpleService);
+        assertEquals(mock(SimpleService.class).getClass(), simpleService.getClass());
     }
 }
